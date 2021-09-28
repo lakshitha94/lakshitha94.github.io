ddc265b3785678829ba56a07cebb496be9dde8c4
lakshitha94 <kasun.amarasinghe@cognite.com>
2021-09-28 20:53:23 +0530

    Updates

---
+++ precache-manifest.7df173026a455dced083fcecfcf1f17f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2422b0d9d5f278f14a75d70a155cbe81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab9fe13d3c369d7854d0",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "b1baf4cad2b98bef187d",
+    "url": "/static/js/2.feeefc50.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.feeefc50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab9fe13d3c369d7854d0",
+    "url": "/static/js/main.8b4ba9a3.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "624669b660b4fb1336f815b5a34089c6",
+    "url": "/static/media/creative.624669b6.png"
+  },
+  {
+    "revision": "2ecb22ad91898d26e22bcae524244c17",
+    "url": "/static/media/kasun.2ecb22ad.jpg"
+  },
+  {
+    "revision": "7b7ed51f24a679a5eb7204bc47210366",
+    "url": "/static/media/resume.7b7ed51f.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.45a01ef3897c605b59bb8d051779bb2f.js"
+  "/precache-manifest.7df173026a455dced083fcecfcf1f17f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
